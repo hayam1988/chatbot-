dc3b1f3cec8c370b79f9c1c660dad4193b89461f
Ashley Hay <hayashley@msn.com>
2019-06-28 20:59:12 -0700

    added send button, message bubbles

--- src/App.js
+++ src/App.js
@@ -10,8 +10,31 @@ function App() {
       <img src = {logo} className = "logo" alt = "logo of chatbot"/>
       </header>
       <TextInput/>
+      <div className = "txtarea"> 
+        <article >  
+
+        <div className="speech-bubble arrow">
+        <div className="arrow"></div>
+        <div className="after"></div>
+            Hi!
+        </div>
+          
+        <div className="speech-bubble ">
+        <div className="arrow"></div>
+        <div className="after"></div>
+            How are you? 
+        </div>
+        <div className="speech-bubble ">
+        <div className="arrow"></div>
+        <div className="after"></div>
+            what time are we meeting today? 
+        </div>
+
+        </article>
+      </div>
     </div>
   );
+
 }
 
 export default App;
--- src/TextInput.js
+++ src/TextInput.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import send from './send2.png'
 class TextInput extends React.Component {
 
     /*is a function*/
     render() {
         return(<div className = "text-input">
-             <input />  
-
-             <button> click here </button>
-
+             <input type = "text" id = "txt1" />
+             <button onclick= "myFunction()">
+             <img src = {send} className = "send" alt = "send message icon button "/>
+             </button>
          </div>)
+  
     }
-}
+} 
+
+
 
 export default TextInput 

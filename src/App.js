36a56a205dee8e414e3710a1bfe8be01aedb7540
Ashley Hay <hayashley@msn.com>
2019-07-02 14:35:43 -0700

    add button icon, hover effect, on click event for the button to send messages when clicked

--- src/App.js
+++ src/App.js
@@ -3,38 +3,28 @@ import './App.css';
 import logo from './logo.png'
 import TextInput from './TextInput'
 
-function App() {
-  return (
-    <div className="App">
-      < header className = "header">chatbot
-      <img src = {logo} className = "logo" alt = "logo of chatbot"/>
-      </header>
-      <TextInput/>
-      <div className = "txtarea"> 
-        <article >  
+class App extends React.Component {
+  state = {
+    messages: []
 
-        <div className="speech-bubble arrow">
-        <div className="arrow"></div>
-        <div className="after"></div>
-            Hi!
-        </div>
-          
-        <div className="speech-bubble ">
-        <div className="arrow"></div>
-        <div className="after"></div>
-            How are you? 
-        </div>
-        <div className="speech-bubble ">
-        <div className="arrow"></div>
-        <div className="after"></div>
-            what time are we meeting today? 
-        </div>
+  }
 
-        </article>
+  sendMessage = (m) => {
+    var message = [...this.state.messages, m]
+    this.setState({ message })
+  }
+
+  render() {
+    console.log(this.state.message)
+    return (
+      <div className="App">
+        < header className="header">chatbot
+      <img src={logo} className="logo" alt="logo of chatbot" />
+        </header>
+        <TextInput sendMessage={this.sendMessage} />
       </div>
-    </div>
-  );
+    );
 
+  }
 }
-
 export default App;
--- src/TextInput.js
+++ src/TextInput.js
@@ -1,19 +1,40 @@
 import React from 'react'
 import send from './send2.png'
+import {IoMdSend} from "react-icons/io";
 class TextInput extends React.Component {
 
-    /*is a function*/
+    /* State is: a java script object */
+    state = {
+       /* text: " Write your message here...."  */
+        text: ""  
+    }
+
+    send = () => {
+            
+        this.props.sendMessage(this.state.text) 
+        this.setState({text: ""})
+    }
+
+    keyPress = (e) => {
+        if (e.key == 'Enter'){
+            this.send()
+        }
+    }
+
     render() {
         return(<div className = "text-input">
-             <input type = "text" id = "txt1" />
-             <button onclick= "myFunction()">
-             <img src = {send} className = "send" alt = "send message icon button "/>
+             <input value ={this.state.text} 
+             onKeyPress = {this.keyPress}
+             /* e=> refers to the event of clicking*/
+             placeholder ="Write your message here..."
+             onChange= {e=> this.setState({text: e.target.value})}
+             />
+             <button disabled = { !this.state.text} onClick = {this.send}
+            >
+                <IoMdSend style={{hieght: 25, width: 25}} />
              </button>
          </div>)
   
     }
 } 
-
-
-
 export default TextInput 
